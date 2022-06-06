d76ef4d96596943535d96ac7ae396ae6a8996a09
Namkat Cedrick j <namkatcedrickjumtock3@gmail.com>
2022-06-06 17:25:57 +0100

    first commit on react v0.0.1

---
+++ src/body/bodyComponent.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Body = () => {
+    
+    return (
+        <div className='container  mx-auto my-5 bg-danger w-50'>
+            <ul>
+                <li>Was first release in 2013</li>
+                <li>Was originally created by Jordan walke</li>
+                <li>Has well as over 100k on Github</li>
+                <li>it is maintained by Facebook</li>
+                <li>Power most Web and Mobile Applications.</li>
+            </ul>
+        </div>
+    );
+}
+
+export default Body;
