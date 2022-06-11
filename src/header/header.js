079d99619ce0cb3b12137345c7a4e1655d1878e8
Namkat Cedrick j <namkatcedrickjumtock3@gmail.com>
2022-06-11 10:00:09 +0100

    abnb sample landing page clone

--- src/header/header.js
+++ src/header/header.js
@@ -4,11 +4,11 @@ const Header = () => {
     return (
         <nav class="navbar navbar-light bg-light">
             <h1 className='text-dark '> Z-index React</h1>
-                <div class="custom-control custom-switch">
-                    <input type="checkbox" class="custom-control-input" id="customSwitch1" />
-                        <label class="custom-control-label" for="customSwitch1">Theme</label>
-                </div>
-        </nav>
+cd and            <div class="custom-control custom-switch">
+                <input type="checkbox" class="custom-control-input" id="customSwitch1" />
+                <label class="custom-control-label" for="customSwitch1">Theme</label>
+            </div>
+        </nav >
     );
 }
 
